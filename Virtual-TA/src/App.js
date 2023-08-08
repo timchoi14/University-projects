cd574d3105c3e1871ff145bffe326557231230f7
timchoi14 <32580201+timchoi14@users.noreply.github.com>
2023-08-08 13:30:45 -0500

    Add files via upload

---
+++ Virtual-TA/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Footer from './footer'
+import Chatbot from "react-chatbot-kit";
+import './main.css';
+import config from "./config";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
+import Form from "./Form.js"
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  
+  return(
+    
+    <div className="App">
+      <div className="form">
+              <Form></Form>
+              </div>
+        <div className = "Bot">
+        <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+      </div>
+      <Footer/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ Virtual-TA/src/MessageParser.js
@@ -0,0 +1,23 @@
+// MessageParser starter code
+import React, { useState, useEffect } from "react";
+
+  const MessageParser = ({ children, actions }) => {
+    const [datas, setdatas] = useState({
+      message:"",
+  })
+    const parse = (message) => {
+        actions.handleDefault(message);
+    };
+  
+    return (
+      <div>
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child, {
+            parse: parse,
+            actions: {},
+          });
+        })}
+      </div>
+    );
+  };
+  export default MessageParser;
